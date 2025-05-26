fc042f6356a32a7554e312db7eafd45bace4d0b4
Mehmet Ali Kodaş <mehmetalekodas@gmail.com>
2025-05-26 07:39:12 +0300

    sa

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5001;
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({status: 'ok'});
-},
+});
 
 
 app.use('/api/transactions', transactionRoute); // Use the transaction routes)
