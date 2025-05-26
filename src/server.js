bdc21cdbcc52753725f7ad36e4d2dde98c85c6de
Mehmet Ali Kodaş <mehmetalekodas@gmail.com>
2025-05-26 07:18:23 +0300

    a

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import rateLimiter from './middleware/rateLimiter.js';
+import rateLimiter from './middleware/ratelimiter.js';
 import transactionRoute from './routes/transactionRoute.js';
 import { connectToDatabase } from './config/db.js'; // Adjust the path as necessary
 dotenv.config();
